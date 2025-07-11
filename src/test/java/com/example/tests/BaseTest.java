dc552ce96423fcc957a45ce0d2f9b268748962bc
Halmurat <73029024+Halmurat-Uyghur@users.noreply.github.com>
2025-07-11 00:03:18 -0400

    Enable parallel JUnit execution

--- src/test/java/com/example/tests/BaseTest.java
+++ src/test/java/com/example/tests/BaseTest.java
@@ -4,27 +4,43 @@ import com.example.framework.PlaywrightModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Page;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public abstract class BaseTest {
     protected static Injector injector;
     protected static Browser browser;
-    protected static Page page;
+    protected BrowserContext context;
+    protected Page page;
 
     @BeforeAll
     static void setUpBase() {
         injector = Guice.createInjector(new PlaywrightModule());
         browser = injector.getInstance(Browser.class);
-        page = browser.newPage();
+    }
+
+    @BeforeEach
+    void setUp() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (page != null) {
+            page.close();
+        }
+        if (context != null) {
+            context.close();
+        }
     }
 
     @AfterAll
     static void tearDownBase() {
-        if (page != null) {
-            page.close();
-        }
         if (browser != null) {
             browser.close();
         }
